38625d333c6c858bbf72665e760af3b5d3455715
ccorr066@fiu.edu <punchcode@Punchcodes-MacBook-Pro.local>
2020-05-26 08:47:09 -0700

    // need to fix it

--- functions.js
+++ functions.js
@@ -1,1 +1,75 @@
-// Your functions go here!
+function add(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 + input2;
+}
+function add(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 + input2;
+}
+function add(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 + input2;
+}
+function add(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 + input2;
+}
+function add(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 + input2;
+}
+function contacts(input1,input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   var celebrity =[
+      {
+         "name": "Johnny Depp",
+         "phone": "760-555-6666"
+      },
+      {
+         "name": " Dwyane Johnson",
+         "phone": "305-786-9543"
+      },
+      {
+         "name": " Tyra Banks",
+         "phone": "463-777-4444 "
+      }
+   ];
+      for(var x=0; x< celebrity.length; x += 1 ){
+      if (celebrity[x].name == input1){
+         if(celebrity[x].phone(input2){
+
+         return celebrity[x][input2];
+      
+      }else {
+         return "No such Celebrity exists";
+      }
+}
+      }
+   }
+}
+
+function headsOrTails(input1) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   var counter = 0;
+   if (input1 == "heads") {
+      counter++;
+   } else if (input1 == "tails") {
+      counter--;
+   }
+   if (counter > 0) return counter + " Heads ";
+   return counter + " Tails ";
+}
