3ddb1f804537915aad6eb5b162d7b3e4f5f87f6f
ccorr066@fiu.edu <punchcode@Punchcodes-MacBook-Pro.local>
2020-05-27 15:06:46 -0700

    added two functions

--- functions.js
+++ functions.js
@@ -22,42 +22,45 @@ function add(input1, input2) {
    // R: a single JavaScript value
    return input1 + input2;
 }
-function add(input1, input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   return input1 + input2;
+function randomRange(myMin, myMax) {
+   return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+   //A1: the function holds a min and max.
+   //R: math.floor rounds the number down while math.random can't reach a number past 1,its in between 0 and 1.
 }
-function contacts(input1,input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   var celebrity =[
-      {
-         "name": "Johnny Depp",
-         "phone": "760-555-6666"
-      },
-      {
-         "name": " Dwyane Johnson",
-         "phone": "305-786-9543"
-      },
-      {
-         "name": " Tyra Banks",
-         "phone": "463-777-4444 "
-      }
-   ];
-      for(var x=0; x< celebrity.length; x += 1 ){
-      if (celebrity[x].name == input1){
-         if(celebrity[x].phone(input2){
 
-         return celebrity[x][input2];
-      
-      }else {
-         return "No such Celebrity exists";
-      }
-}
+var celebrity = [
+   {
+      name: "Johnny Depp",
+      phone: "760-555-6666",
+   },
+   {
+      name: "Dwyane Johnson",
+      phone: "305-786-9543",
+   },
+   {
+      name: "Tyra Banks",
+      phone: "463-777-4444 ",
+      likes: ["beaches", "beer", "glasses"],
+   },
+];
+function contacts(name1, prop) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   var num = "";
+   var text = "";
+   for (var x = 0; x < celebrity.length; x++) {
+      if (celebrity[x].name === name1) {
+         if (celebrity[x].hasOwnProperty(prop)) {
+            num = celebrity[x][prop];
+         } else {
+            text = "No such Property";
+         }
+      } else {
+         text = "No such celebrity exists";
       }
    }
+   return num || text;
 }
 
 function headsOrTails(input1) {
